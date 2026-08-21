442e9f79d70d907b9fffe9babfda70864449e2bc
Ashika <2023ucp1671@mnit.ac.in>
2026-08-21 14:21:15 +0530

    Implemented Shift Cipher.

---
+++ attacks/shift_cipher_attack/src/shift_cipher.py
@@ -0,0 +1,12 @@
+def encrypt(text, key):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            shift = 65 if char.isupper() else 97
+            result += chr((ord(char) - shift + key) % 26 + shift)
+        else:
+            result += char
+    return result
+
+def decrypt(text, key):
+    return encrypt(text, 26 - (key % 26))
